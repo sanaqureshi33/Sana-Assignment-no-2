af3b7c19cea7c97eb893bf0b1fc489ff95d38699
sanaqureshi33 <94166252+sanaqureshi33@users.noreply.github.com>
2021-11-18 15:40:54 +0500

    Add files via upload

---
+++ Ass3.dart
@@ -0,0 +1,78 @@
+import 'dart:io';
+
+
+void main(){
+     //Question No 1
+  // String Title ="Dart Course";
+  // String v1 =Title[0].toUpperCase()+Title.substring(1).toLowerCase();
+  // print(v1); 
+
+
+  // String Title ="Dart Course";
+  // String v1 =Title.toUpperCase();
+  // print(v1);
+
+  // String Title ="Dart Course";
+  // String v1 =Title.toLowerCase();
+  // print(v1);
+
+
+  // //QUESTION NO 2
+  //  stdout.write("Enter Number");
+  //  int? v1=int.parse(stdin.readLineSync()!);  
+  //  print(v1+5*(10/2));
+
+
+
+//   // QUESTION NO 3
+//     int a = 2;
+//     int b = 1;
+// print(--a);
+// print(--a - --b);
+// print(--a - --b + ++b);
+// print(--a - --b + ++b + b--);
+
+
+  //QUESTION NO 4
+  // String value1 ="SANA";
+  // String value2 ="SaNA";
+  // print(value1.compareTo(value2));
+
+
+  //QUESTION NO 5
+  // stdout.write("Enter Number");
+  // String? n1=stdin.readLineSync()!;
+  
+  // print(n1.length);
+
+  //QUESTION NO 6
+  // String d1 = "ABC";
+  // int a1= d1.codeUnitAt(0);
+  // int a2= d1.codeUnitAt(1);
+  // int a3= d1.codeUnitAt(2);
+  // print(a1);
+  // print(a2);
+  // print(a3);
+
+
+//QUESTION NO 7
+  
+  // stdout.write("Enter Number");
+  // String noor=stdin.readLineSync()!;
+  
+  // print(noor.substring(4,7));
+
+//QUESTION NO 8
+
+// String a1 = "sana.areeba.shehla";
+// var v2 = a1.split('.');
+// print(v2);
+
+  
+
+
+   
+
+
+}
+
